02a4504a6deeae4a2e27447f82ba19f3ba0c90ec
xhepower <hbuezoss@gmail.com>
2022-11-14 23:01:32 -0600

    format laptop

---
+++ handlePdf.js
@@ -0,0 +1,81 @@
+import pdfParse from 'pdf-parse';
+import { exportImages } from 'pdf-export-images';
+import * as fs from 'fs';
+import * as utils from './utils.js';
+const pdf1 = './pdfs/ANGIE NAVARRETE (3rd copy).pdf';
+const pdf2 =
+  './pdfs/Consular Electronic Application Center - Print Applicationema.pdf';
+
+const getContent = async (pdf) => {
+  return (await pdfParse(pdf)).text
+    .split('\n')
+    .toString()
+    .split(',')
+    .toString()
+    .split(',')
+    .toString()
+    .split(':')
+    .toString()
+    .split(',')
+    .toString()
+    .split('?')
+    .toString()
+    .split(',')
+    .filter((item) => item !== '');
+};
+export async function esDigno(pdf) {
+  const contenido = await getContent(pdf);
+  if (contenido.includes('Application - Sensitive But Unclassified(SBU)')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+export const nombreDeArchivo = (datos) => {
+  let guionesbajos = datos.name.split(' ').join('_');
+  const nombre = `usol-${utils.fechaDDMMAAAA(datos.date)}-${guionesbajos}-${
+    datos.idNumber
+  }-${datos.phone}-${datos.passport}.pdf`;
+  return nombre;
+};
+export async function datos(pdf) {
+  const datosBusqueda = {
+    name: 'Full Name in Native Language',
+    idNumber: 'National Identification Number',
+    city: 'City',
+    address: 'Home Address',
+    phone: 'Primary Phone Number',
+    email: 'Email Address',
+    passport: 'Passport/Travel Document Number',
+    purpose: 'Purpose of Trip to the U.S. (1)',
+    issued: 'Have you ever been issued a U.S. visa',
+  };
+  const content = await getContent(pdf);
+  let datos = {};
+
+  datos.date = (() => {
+    let datePieces = content[0].split('/');
+    datePieces[2] = parseInt(datePieces[2]) + 2000;
+    return new Date(datePieces[2], datePieces[1] - 1, datePieces[0]);
+  })();
+
+  await Promise.all(
+    Object.keys(datosBusqueda).map((key) => {
+      datos[key] = (() => {
+        for (let i = 0; i <= content.length; i++) {
+          if (content[i] == datosBusqueda[key]) {
+            return content[i + 1];
+          }
+        }
+      })();
+    })
+  );
+  datos.file = nombreDeArchivo(datos);
+  if (datos.issued !== 'NO') {
+    datos.estado = 'proceso';
+  }
+  return datos;
+}
+(async () => {
+  // console.log((await datos(pdf1)).name);
+})();
--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-const ip = require("ip")
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
-const fs = require("fs");
-const { json } = require("express");
+import { datos, esDigno, nombreDeArchivo } from './handlePdf.js';
+import * as fs from 'fs';
+import rimraf from 'rimraf';
+import * as dpath from 'path';
+const path = './pdfs';
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get('/', function (req, res, next) {
-    res.send("HOME");
-});
+//las variables a imprimir
+let archivosNoPdf = 0;
+const purgar = async () => {
+  let files = await Promise.all(
+    fs
+      .readdirSync(path)
+      .filter((file) =>
+        fs.statSync(`${path}/${file}`).isDirectory() ||
+        dpath.extname(file).toLowerCase() !== '.pdf'
+          ? false
+          : true
+      )
+      .map(async (pdf) => ((await esDigno(`${path}/${pdf}`)) ? pdf : null))
+  );
+  let uniqueFiles = [];
+  files = files.filter((file) => {
+    return file == null ? false : true;
+  });
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${ip.address()}:${PORT}`);
+  files = await Promise.all(
+    files.map(async (pdf) => {
+      let nombre = (await datos(`${path}/${pdf}`)).file;
+      if (uniqueFiles.includes(nombre)) {
+        return null;
+      } else {
+        uniqueFiles.push(nombre);
+        return pdf;
+      }
+    })
+  );
+  files = files.filter((file) => {
+    return file == null ? false : true;
+  });
+  return files;
+};
 
-});
+(async () => {
+  console.log(await purgar());
+})();
